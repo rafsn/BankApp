42a01b12c34ea0798b3e6991f8e4b14d55147353
rafsn <santos_rafa1973@hotmail.com>
2023-12-08 22:15:18 -0300

    Add NavigationBar

--- app/src/main/java/com/projeto/bankapp/MainActivity.kt
+++ app/src/main/java/com/projeto/bankapp/MainActivity.kt
@@ -3,13 +3,23 @@ package com.projeto.bankapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.projeto.bankapp.ui.theme.BankAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +27,49 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BankAppTheme {
-                // A surface container using the 'background' color from the theme
+                SetBarColor(color = MaterialTheme.colorScheme.background)
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
     }
+
+    @Composable
+    private fun SetBarColor(color: Color) {
+        val systemUiController = rememberSystemUiController()
+        SideEffect {
+            systemUiController.setSystemBarsColor(
+                color = color
+            )
+        }
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HomeScreen() {
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar()
+        }
+    ) { padding ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+//        WalletSection()
+//        CardSection()
+            Spacer(modifier = Modifier.height(16.dp))
+//        FinanceSection()
+//        CurrenciesSection()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BankAppTheme {
-        Greeting("Android")
+        }
+
     }
 }
